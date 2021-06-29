456c3d14fdae8127ef6e09d169fbfcc36c7405df
Joris Scharp <jorisscharp@gmail.com>
2021-06-29 11:31:39 +0200

    Added a direct call that requests the Bearer token. This endpoint can also be used for testing applications to easily fetch an access token (only requirement is that the client_id is an approved SMART backend service)

--- src/main/java/nl/koppeltaal/smart/testsuite/Application.java
+++ src/main/java/nl/koppeltaal/smart/testsuite/Application.java
@@ -11,6 +11,9 @@ package nl.koppeltaal.smart.testsuite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Spring Boot application entry point.
@@ -18,6 +21,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/nl/koppeltaal/smart/testsuite/controllers/SmartTestSuiteLaunchController.java
+++ src/main/java/nl/koppeltaal/smart/testsuite/controllers/SmartTestSuiteLaunchController.java
@@ -8,13 +8,24 @@
 
 package nl.koppeltaal.smart.testsuite.controllers;
 
-import nl.koppeltaal.smart.testsuite.service.SmartTestSuiteService;
-import nl.koppeltaal.smart.testsuite.valueobject.SmartTestSuiteLaunchRequest;
-import nl.koppeltaal.smart.testsuite.valueobject.LaunchData;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.security.GeneralSecurityException;
+import java.util.Collections;
+import nl.koppeltaal.smart.testsuite.service.SmartTestSuiteService;
+import nl.koppeltaal.smart.testsuite.valueobject.LaunchData;
+import nl.koppeltaal.smart.testsuite.valueobject.SmartTestSuiteLaunchRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -23,6 +34,13 @@ import java.security.GeneralSecurityException;
 @RestController
 @RequestMapping("launch")
 public class SmartTestSuiteLaunchController {
+
+	private final RestTemplate restTemplate;
+
+	public SmartTestSuiteLaunchController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
 	@Autowired
 	public void setSmartTestSuiteService(SmartTestSuiteService smartTestSuiteService) {
 		this.smartTestSuiteService = smartTestSuiteService;
@@ -30,11 +48,34 @@ public class SmartTestSuiteLaunchController {
 
 	private SmartTestSuiteService smartTestSuiteService;
 
-	@RequestMapping(method = RequestMethod.POST)
-	public LaunchData launch(@RequestBody SmartTestSuiteLaunchRequest request) throws GeneralSecurityException {
+	@RequestMapping(value = "debug", method = RequestMethod.POST)
+	public LaunchData debug(@RequestBody SmartTestSuiteLaunchRequest request) throws GeneralSecurityException {
 		LaunchData rv = new LaunchData(request, false);
 		rv.setClientAssertion(smartTestSuiteService.getLaunchToken(request));
 		return rv;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String getAccessToken(@RequestBody SmartTestSuiteLaunchRequest launchRequest) throws GeneralSecurityException, JSONException {
+		LaunchData rv = new LaunchData(launchRequest, false);
+		rv.setClientAssertion(smartTestSuiteService.getLaunchToken(launchRequest));
+
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+		body.add("client_assertion_type", rv.getClientAssertionType());
+		body.add("client_assertion", rv.getClientAssertion());
+		body.add("grant_type", rv.getGrantType());
+
+		if(StringUtils.isNotBlank(rv.getScope())) {
+			body.add("scope", rv.getScope());
+		}
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		final HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+		return restTemplate.postForObject(rv.getUrl(), request, String.class);
 	}
 
 }
